34bec4b366aef0a37334939d44c0f4699316a988
Dract0r <dib.martinez@duocuc.cl>
2024-07-11 00:43:48 -0400

    v1

---
+++ Carrito/CarritoApp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Producto  
+from .carrito import Carrito  
+
+
+def tienda(request):
+    productos = Producto.objects.all()
+    return render(request, "tienda.html", {'productos': productos})
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("tienda")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)  
+    return redirect("tienda")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("tienda")
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("tienda")
